d5208a1bcbc90d8065d70b7806ed27a9f50b1c0f
Lyebon <sebas25garcia@gmail.com>
2025-01-24 20:26:37 +0000

    FPS

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         screen.fill("black")
@@ -17,7 +19,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60)
+        dt = dt/1000
 
 if __name__ == "__main__":
     main()
-    
