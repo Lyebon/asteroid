94d93a2e66e6cceef2e20e4be1104d4bc3ea45df
Lyebon <sebas25garcia@gmail.com>
2025-01-24 20:11:19 +0000

    lawea

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-#bootdev
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
+import pygame
+
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+if __name__ == "__main__":
+    main()
+    
