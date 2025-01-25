1bd99142ef5b7ceef558df1da2e2fdc9b2471dcb
Lyebon <sebas25garcia@gmail.com>
2025-01-25 21:25:49 +0000

    el jugador

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 import pygame
-
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    player = Player(x, y)
+    
     while True:
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60)
         dt = dt/1000
+        
+        
 
 if __name__ == "__main__":
     main()
